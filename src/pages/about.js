3851c656cf851096ca1533e9baf45f0f4440ec71
Shoaib Rakhangi <97998577+sho-r1024@users.noreply.github.com>
2023-06-07 00:37:50 -0400

    updated about me section, fixed some weird wording

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,14 +13,14 @@ const AboutPage = () => (
             <p>
                 Originally from NYC, I am an undergrad at Northeastern University c'24 studying CS with a Systems concentration. 
                 During my 3 years here, I've worked with so many different technologies ranging from socket programming in Python
-                to OOD with Java and Mutex Programming in C. Over my time, I've developed a deep interest in Networks and Systems and a deep
+                to OOD with Java and Mutex Programming in C. Over my time, I've developed a deep interest in networks and systems and a deep
                 love for the art of coding. As someone went into CS with zero prior programming knowledge, there's still a long way for me
-                to go, but I've learned so, so much and I am looking forward to blossoming further and finding my own voice in tech.
+                to go, but I've learned so, so much and I am looking forward to blossoming even further and finding my own voice in tech.
             </p>
             <p>
                 In my spare time, I enjoy diving deep into music of all types, going to concerts, riding public transportation &#40;I am from NYC after all!&#41;, 
                 and travelling to all the less-talked-about places around me. I've always enjoyed dipping my toes into something new, whether it be a new
-                programming language, a new neighborhood, or a new album.  One thing I love the most about myself, I never stick with one thing for too long. I made this 
+                programming language, a new neighborhood, or a new album.  One thing I love the most about myself is that I never stick with one thing for too long. I made this 
                 website in the hopes that I can showcase my ever-changing interests. So please feel free to look around, I hope you have a fun time browsing through!
             </p>
         </Layout>
